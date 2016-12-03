687cf47d14834e005ad0c8c1e017f64178237b2c
Giovanni Grano <granogiovanni90@gmail.com>
2016-12-03 14:41:10 +0100

    Warnings

--- src/main/java/extractors/ReviewExtractor.java
+++ src/main/java/extractors/ReviewExtractor.java
@@ -3,7 +3,6 @@ package extractors;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 import config.ConfigurationManager;
 import crawler.Crawler;
